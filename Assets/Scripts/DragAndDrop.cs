1ac0396fe2f068d3b642cf66b37eb18a829a3984
Arda Özler <ardaozler@yahoo.com>
2025-06-01 13:16:51 +0200

    feat: update texture settings, drag and drop added to food

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Vector3 _offset;
+    private float _fixedY;
+    
+    private Vector3 _originalScale;
+
+    private void OnMouseDown()
+    {
+        _fixedY = transform.position.y;
+
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z)
+        );
+
+        _offset = transform.position - mouseWorldPoint;
+    }
+
+    
+    private void OnMouseUp()
+    {
+        // Reset scale
+        transform.localScale = _originalScale;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z)
+        );
+
+        Vector3 targetPosition = mouseWorldPoint + _offset;
+        targetPosition.y = _fixedY;
+
+        transform.position = targetPosition;
+    }
+}
