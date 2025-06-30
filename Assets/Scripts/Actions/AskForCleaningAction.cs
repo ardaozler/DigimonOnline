cb82ec396e39a1f06760d989a597311a7423a5f2
Arda Özler <ardaozler@yahoo.com>
2025-06-30 17:22:30 +0200

    demo 1

--- Assets/Scripts/Actions/AskForCleaningAction.cs
+++ Assets/Scripts/Actions/AskForCleaningAction.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 //TODO: add it as an urge action
 public class AskForCleaningAction : DigimonAction
 {
-    private int x = 10000;
-    private float _cleaningRequestTimer = 3f;
     public override bool Act(ActContext actContext, Action onActionCompleted)
     {
         //sits and asks for cleaning, block the action queue, wait indefinitely for the player to clean
-
-        //start the animation
         
+        if (actContext is not GameObjectContext context)
+        {
+            Debug.LogError("AskForCleaningAction requires a GameObjectContext.");
+            return false;
+        }
+        var mover = context.Agent.GetComponent<DigimonMover>();
+        if (mover == null)
+        {
+            Debug.LogError("AskForCleaningAction requires a DigimonMover component on the agent.");
+            return false;
+        }
+        
+        // Set the movement strategy to sit
         
         return base.Act(actContext, onActionCompleted);
     }
